38cb74cfe96c05444ffd6c0c35b0e307b3ba976e
DESKTOP-0SRFR83\ayome <andreas.kuhn@bluewin.ch>
2025-10-24 14:17:44 +0200

    finished exercise 2: log-levels

--- exercism/cpp/log-levels/log_levels.cpp
+++ exercism/cpp/log-levels/log_levels.cpp
@@ -1,15 +1,25 @@
 #include <string>
 
 namespace log_line {
+
 std::string message(std::string line) {
-    // return the message
+    // return the message (the part after '[INFO]:', '[WARNING]:', '[ERROR]:')
+    size_t idx_message_start = line.find("]: ") + 3; // +3 for moving at end of found string
+    return line.substr(idx_message_start, line.size());
 }
 
 std::string log_level(std::string line) {
     // return the log level
+    size_t idx_level_start = line.find("[")+1;
+    size_t idx_level_end = line.find("]: ")-1;
+    
+    return line.substr(idx_level_start, idx_level_end);
 }
 
 std::string reformat(std::string line) {
     // return the reformatted message
+    std::string reformatted = message(line) + " (" + log_level(line) + ")";
+    return reformatted;
 }
+
 }  // namespace log_line
--- exercism/cpp/log-levels/log_levels_test.cpp
+++ exercism/cpp/log-levels/log_levels_test.cpp
@@ -7,6 +7,7 @@
 
 using namespace std;
 
+// Unit Tests
 TEST_CASE("Error message", "[task_1]") {
     const string actual = log_line::message("[ERROR]: Stack overflow");
 
@@ -15,7 +16,6 @@ TEST_CASE("Error message", "[task_1]") {
     REQUIRE(expected == actual);
 }
 
-#if defined(EXERCISM_RUN_ALL_TESTS)
 TEST_CASE("Warning message", "[task_1]") {
     const string actual = log_line::message("[WARNING]: Disk almost full");
 
@@ -55,7 +55,7 @@ TEST_CASE("Info log level", "[task_2]") {
 
     REQUIRE(actual == expected);
 }
-
+#if defined(EXERCISM_RUN_ALL_TESTS)
 TEST_CASE("Error reformat", "[task_3]") {
     const string actual = log_line::reformat("[ERROR]: Segmentation fault");
 
